cecc283ca8832a088128c4161dd5cc6a11b0d1bc
Aleksandr Sladkov <savq85@gmail.com>
2026-09-07 20:32:10 +0300

    fix6

--- fit_life.py
+++ fit_life.py
@@ -1,12 +1,23 @@
 import io
 import sys
 
+
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 sys.stdin = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
 
 
+MIN_AGE = 10
+MAX_AGE = 100
+MIN_WIEGHT = 10
+MAX_WIEGHT = 200
+MIN_HEIGHT = 1.00
+MAX_HIEGHT = 3.00
+WATER_ML_KG = 30
+ML_IN_LITTER = 1000
+
+
 while True:
-    user_name = input("Введите ваше имя:")
+    user_name = input("Введите ваше имя:").strip()
     if not user_name:
         print("Введите имя")
     elif not user_name.isalpha():
@@ -18,7 +29,7 @@ while True:
 while True:
     try:
         user_age = int(input("Введите ваш возраст:"))
-        if 0 <= user_age <= 100:
+        if MIN_AGE <= user_age <= MAX_AGE:
             break
     except ValueError:
         print("Пожалуйста, введите ваш возраст")
@@ -27,7 +38,7 @@ while True:
 while True:
     try:
         user_weight = float(input("Введите ваш вес (например 100 кг):"))
-        if 1 <= user_weight <= 200:
+        if MIN_WIEGHT <= user_weight <= MAX_WIEGHT:
             break
     except ValueError:
         print("Пожалуйста, введите вас вес")
@@ -36,7 +47,7 @@ while True:
 while True:
     try:
         user_height = float(input("Введите ваш рост (например 1.75):"))
-        if 1 <= user_height <= 250:
+        if MIN_HEIGHT <= user_height <= MAX_HIEGHT:
             break
     except ValueError:
         print("Пожалуйста, введите ваш рост в метрах")
@@ -44,16 +55,14 @@ while True:
 
 def calculate_bmi(user_weight, user_height):
     """Функция для расчета ИМТ"""
-    if user_height <= 0:
-        return None
     return user_weight / (user_height ** 2)
 
 
 result_bmi = calculate_bmi(user_weight, user_height)
 
 
-water_ml = user_weight * 30
-water_l = water_ml / 1000
+water_ml = user_weight * WATER_ML_KG
+water_l = water_ml / ML_IN_LITTER
 
 
 print(f"Отчет для пользователя: {user_name} ({user_age}) лет!")
